d92e9632103c069ac59361e2c539fed9bde8fbe6
Yousif <ybeeai@gmail.com>
2021-10-11 19:56:26 -0400

    first commit

---
+++ CircleTester/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CircleTester
+{
+    class Circle
+    {
+        //properties
+        private double Radius;
+        ////getter = a way to see the radius without changing it
+        public double radius
+        {
+            get { return Radius; }
+        }
+        //constructor
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        //methods
+        public double CalculateCircumference()
+        {
+            return 2 * radius * Math.PI;
+        }
+        public double CalculateArea()
+        {
+            return 2 * Math.PI * (radius * radius);
+        }
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());     
+        }
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());       
+        }
+        private string FormatNumber(double x)
+        {
+            return Math.Round(x,2).ToString();
+        }
+    }
+}
---
+++ CircleTester/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CircleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Welcome to the Circle Tester");
+            bool runProgram = true;
+            List<Circle> inputList = new List<Circle>();
+            while (runProgram)
+            {
+                Console.WriteLine("Enter radius");
+                try
+                {
+                    double radius = double.Parse(Console.ReadLine());
+                    if (radius <= 0)
+                    {
+                        Console.WriteLine("Please enter a number more than 0");
+                    }
+                    else if (radius > 0)
+                    {
+                        Circle circle = new Circle(radius);
+                        count++;
+                        inputList.Add(circle);
+                        Console.WriteLine("Circumference: " + circle.CalculateFormattedCircumference());
+                        Console.WriteLine("Area:" + circle.CalculateFormattedArea());
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                runProgram = GetContinue();
+                if (runProgram == false)
+                {
+                    Console.WriteLine($"Goodbye. You created {inputList.Count} circles.");
+                }
+            }
+        }
+        static bool GetContinue()
+        {
+            bool result = true;
+            while (true)
+            {
+                Console.WriteLine("Would you like to continue? y/n");
+                String input = Console.ReadLine().ToLower();
+                if (input == "y")
+                {
+                    result = true;
+                    break;
+                }
+                else if (input == "n")
+                {
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("That wasn't a valid input.");
+                }
+            }
+            return result;
+        }
+    }
+}
